946daba7d7132e9b97346341d19bccc1add50cf2
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-01-15 18:23:35 +0000

    feat: add ChatWindow component as main chat container

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
-
+import ChatWindow from "./components/ChatWindow.jsx";
 function App() {
-  const [message, setMessage] = useState([
-    { id: 1, user: 'Rahwa', text: 'Hi there!', timeStamp: "10:00 AM" },
-    { id: 2, user: 'Selam', text: 'Hello!', timeStamp: "10:01 AM" }
-  ]);
-
   return (
     <div className="flex justify-center items-start min-h-screen bg-gray-100 p-6">
-      <div className="w-full max-w-md p-4 bg-white shadow-md rounded">
-        <h1 className="text-center text-xl font-bold mb-4">Chat App</h1>
-        <ul>
-          {message.map((mse) => (
-            <li key={mse.id}>
-              <strong>{mse.user}</strong>: {mse.text} <em>({mse.timeStamp})</em>
-            </li>
-          ))}
-        </ul>
+      <div className="w-full max-w-sm h-[700px] bg-white shadow-xl rounded-3xl flex flex-col overflow-hidden">
+        <ChatWindow> 
+          
+        </ChatWindow>
       </div>
     </div>
   );
---
+++ Frontend/src/components/ChatWindow.jsx
@@ -0,0 +1,9 @@
+
+function ChatWindow({children}){
+    return(
+        <div className="flex flex-col h-full">
+            {children} 
+        </div>  
+    )
+}
+export default ChatWindow;
