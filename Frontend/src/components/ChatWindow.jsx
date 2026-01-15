f7586985637d119e3759631b1cf91718a5d1cc03
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-01-15 19:09:48 +0000

    feat: add Header component for ChatWindow top bar

--- Frontend/src/components/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import Header from "./Header.jsx";
 
 function ChatWindow({children}){
     return(
         <div className="flex flex-col h-full">
-            {children} 
+            <Header />
+            <div className="flex-1 overflow-y-auto p-4">
+                {children} 
+            </div>
         </div>  
     )
 }
